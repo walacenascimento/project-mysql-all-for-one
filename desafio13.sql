bdb96cc86799c0574c0f7f63a3257f4dcdfdc386
Walace Nascimento <wlacens@hotmail.com>
2021-10-24 08:39:24 -0300

    desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,4 @@
 -- 13 - Mostre o supplier_id das purchase_orders em que o supplier_id seja 1 ou 3.
 
-SELECT * FROM northwind.purchase_orders
+SELECT supplier_id FROM northwind.purchase_orders
 WHERE supplier_id = 1 OR supplier_id = 3 ;
