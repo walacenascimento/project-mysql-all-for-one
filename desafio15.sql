91d44fec58748043f4cecfd22462628a287965f2
Walace Nascimento <wlacens@hotmail.com>
2021-10-24 08:05:12 -0300

    Resolve o desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,6 @@
+-- 15 - Mostre somente as horas (sem os minutos e os segundos) da coluna submitted_date 
+-- de todos registros da tabela purchase_orders.
+-- Observações técnicas - Chame essa coluna de submitted_hour.
+
+SELECT HOUR(submitted_date) FROM northwind.purchase_orders 
+AS submitted_hour;
